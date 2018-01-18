da5c28c0d7122b47edabb19cf78de62fa66c2de2
Alvin Richards <alvin.richards@mac.com>
2018-01-18 11:50:47 -0800

    Fixed bug in pipeline, seperating the immediate from the pipeline for the setnx operations.

--- redis/inventory/check_inventory.py
+++ redis/inventory/check_inventory.py
@@ -32,7 +32,6 @@ def create_event(event_name, rows, seats_per_row):
 		row_name = increment_str(row_name)
 
 def get_event_seat_row(event_name, row_name):
-	print event_name, row_name
 	return struct.unpack('l', redis.get("events:" + event_name + ":" + row_name))[0]
 
 def print_event_seat_map(event_name):
@@ -52,14 +51,14 @@ event = "Judo"
 create_event(event, 2, 20)
 print_event_seat_map(event)
 
-def check_availbale(seat_map, seats_required, first_seat=-1):
+def get_availbale(seat_map, seats_required, first_seat=-1):
 	seats = []
 	if ( first_seat != -1 ):
 		end_seat = first_seat + seats_required -1
 	else:
-		end_seat = seat_map.bit_length()
+		end_seat = seat_map.bit_length()+1
 	required_block = int(math.pow(2,seats_required))-1
-	for i in range(1, end_seat):
+	for i in range(1, end_seat+1):
 		if ( (seat_map & required_block) == required_block ):
 			seats.append( {'first_seat': i, 'last_seat': i + seats_required -1} )
 		required_block = required_block << 1
@@ -72,7 +71,7 @@ def find_seat_selection(event_name, seats_required):
 	for row in rows:
 		(_, row_name) = row.rsplit(":",1)
 		seat_map = get_event_seat_row(event_name, row_name)
-		row_blocks = check_availbale(seat_map, seats_required)
+		row_blocks = get_availbale(seat_map, seats_required)
 		if (len(row_blocks) > 0):
 			seats.append( {'event': event_name, 'row': row_name, 'available': row_blocks } )
 	return seats
@@ -95,20 +94,34 @@ def generate_order_id():
   return ''.join(random.choice(string.ascii_uppercase + string.digits) \
     for _ in range(6))
 
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+class SeatTaken(Error):
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
+
 def reservation(event_name, row_name, first_seat, last_seat):
 	reserved = False
 	redis.watch("events:" + event_name + ":" + row_name)
 	p = redis.pipeline()
 	try:
-		order_id = generate_order_id()
-		required_block = int(math.pow(2,last_seat - first_seat + 1))-1 << (first_seat-1)
-		for i in range(last_seat - first_seat +1):
-			p.setnx("orders:" + event_name + ":" + row_name + ":" + str(i), True)
-			p.expire("orders:" + event_name + ":" + row_name + ":" + str(i), 5)
-		p.psetex("orders:" + event_name + ":" + row_name + ":" + order_id, 5000, struct.pack('l',required_block))
 		seat_map = get_event_seat_row(event_name, row_name)
-		seats = check_availbale(seat_map, last_seat - first_seat + 1, first_seat)
+		seats = get_availbale(seat_map, last_seat - first_seat + 1, first_seat)
 		if ( len(seats) > 0 ):
+			for i in range(first_seat, last_seat+1):
+				# Reserve individual seat, raise exception is already reserved
+				if (redis.set("orders:" + event_name + ":" + row_name + ":" + str(i), 
+					            True,
+					            px=5000, nx=True) != True):
+					raise SeatTaken(i, "orders:" + event_name + ":" + row_name + ":" + str(i))
+			order_id = generate_order_id()
+			required_block = int(math.pow(2,last_seat - first_seat + 1))-1 << (first_seat-1)
+			redis.set("orders:" + event_name + ":" + row_name + ":" + order_id, 
+				        struct.pack('l',required_block),
+				        px=5000, nx=True)
 			p.bitop("XOR", "events:" + event_name + ":" + row_name, 
 				             "events:" + event_name + ":" + row_name, 
 				             "orders:" + event_name + ":" + row_name + ":" + order_id)
@@ -116,6 +129,8 @@ def reservation(event_name, row_name, first_seat, last_seat):
 			reserved = True
 	except WatchError:
 		print "Write Conflict: {}".format("events:" + event_name + ":" + row_name)
+	except SeatTaken as error:
+		print "Seat Taken/{}".format(error.message)
 	finally:
 		p.reset()
 	return reserved
@@ -125,21 +140,33 @@ create_event(event, 1, 10)
 # Seat 4 (the 8th bit) is already sold. We calc this as (2^(seats)-1) - bit_number_of_seat, e.g. 1023 - 8
 set_seat_map(event, "A", 1023-8)
 print_event_seat_map(event)
+
 seats = find_seat_selection(event, 2)
 print_seat_availbailiy(seats)
 # Just choose the first found
-made_reservation = reservation(event, "A", seats[0]['available'][0]['first_seat'], seats[0]['available'][0]['last_seat'])
+made_reservation = reservation(event, seats[0]['row'], seats[0]['available'][0]['first_seat'], seats[0]['available'][0]['last_seat'])
 print "Made reservation? {}".format(made_reservation)
 print_event_seat_map(event)
 
+# Find space for 5 seats
 seats = find_seat_selection(event, 5)
 print_seat_availbailiy(seats)
 # Just choose the first found
-made_reservation = reservation(event, "A", seats[0]['available'][0]['first_seat'], seats[0]['available'][0]['last_seat'])
+made_reservation = reservation(event, seats[0]['row'], seats[0]['available'][0]['first_seat'], seats[0]['available'][0]['last_seat'])
 print "Made reservation? {}".format(made_reservation)
 print_event_seat_map(event)
 
+# Find space for 2 seat, but not enough inventory
+seats = find_seat_selection(event, 2)
+if ( len(seats) == 0 ):
+	print "Not enough seats"
 
-
+# Find space for 1 seat
+seats = find_seat_selection(event, 1)
+# Create a seat reservation (simulating another user), so that the reservation fails
+redis.set("orders:" + event + ":" + seats[0]['row'] + ":" + str(seats[0]['available'][0]['first_seat']), True)
+made_reservation = reservation(event, seats[0]['row'], seats[0]['available'][0]['first_seat'], seats[0]['available'][0]['last_seat'])
+print "Made reservation? {}".format(made_reservation)
+print_event_seat_map(event)
 
 
